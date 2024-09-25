6bc36a3bc374054c45e7f8e81601e068b0644d0c
Dmitry Belkin <dmitriy.belkin@rubius.com>
2024-09-25 20:54:00 +0300

    feat: 🎸 Add solution for CalculatorApp

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -1,26 +1,79 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CalculatorApp',
 
-  setup() {},
+  setup() {
+    const operators = [
+      {
+        value: 'sum',
+        label: '➕',
+        callback: (a, b) => a + b,
+      },
+      {
+        value: 'subtract',
+        label: '➖',
+        callback: (a, b) => a - b,
+      },
+      {
+        value: 'multiply',
+        label: '✖️',
+        callback: (a, b) => a * b,
+      },
+      {
+        value: 'divide',
+        label: '➗',
+        callback: (a, b) => a / b,
+      },
+    ]
+
+    const leftOperator = ref(0)
+    const rightOperator = ref(0)
+    const operator = ref(operators[0].value)
+
+    const __callbacks = operators.reduce((acc, { value, callback }) => {
+      acc[value] = callback
+      return acc
+    }, {})
+
+    const result = computed(() => {
+      const a = leftOperator.value
+      const b = rightOperator.value
+      const calcCallback = __callbacks[operator.value]
+
+      if (!calcCallback) {
+        throw new Error(`Unsupported operator: ${operator.value}`)
+      }
+
+      return calcCallback(a, b)
+    })
+
+    return {
+      operators,
+      leftOperator,
+      rightOperator,
+      operator,
+      result,
+    }
+  },
 
   template: `
     <div class="calculator">
-      <input type="number" aria-label="First operand" />
+      <input v-model="leftOperator" type="number" aria-label="First operand"/>
 
       <div class="calculator__operators">
-        <label><input type="radio" name="operator" value="sum"/>➕</label>
-        <label><input type="radio" name="operator" value="subtract"/>➖</label>
-        <label><input type="radio" name="operator" value="multiply"/>✖</label>
-        <label><input type="radio" name="operator" value="divide"/>➗</label>
+        <label v-for="o in operators">
+          <input v-model="operator" :checked="o.value === operator" type="radio" name="operator"
+                 :value="o.value"/>
+          {{ o.label }}
+        </label>
       </div>
 
-      <input type="number" aria-label="Second operand" />
+      <input v-model="rightOperator" type="number" aria-label="Second operand"/>
 
       <div>=</div>
 
-      <output>0</output>
+      <output>{{ result }}</output>
     </div>
   `,
 })
